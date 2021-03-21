ba14f78cf37cecb1532b2ad3f72305395ceaf5fe
chiefanunzio <chiefa.nunzio@gmail.com>
2021-03-21 23:09:29 +0100

    fix ticket

--- js/script.js
+++ js/script.js
@@ -3,25 +3,29 @@ var annullaBtn = document.getElementById('ann');
 var costo = 0.21;
 var costoBiglietto = 0;
 
-generaBtn.addEventListener('click', function(){
-    var name = document.getElementById('name-ip').value;
-    var distanza = parseInt(document.getElementById('number-ip').value);
-    var eta = document.getElementById('select-ip').value;
+generaBtn.addEventListener('click', function () {
+  var distanza = parseInt(document.getElementById('number-ip').value);
+  var name = document.getElementById('name-ip').value;
+  var eta = document.getElementById('select-ip').value;
 
-    if (name !== '' && distanza > 0 && eta !== '') {
-      console.log('apposto');
-    } else {
-      console.log('ngul');
-    }
+  if (name !== '' && distanza > 0 ) {
+    console.log('apposto');
+  } else {
+    console.log('ngul');
+  }
 
-    costoBiglietto = costo * distana;
+  costoBiglietto = costo * distanza;
 
-     if (eta == 1){
-       console.log('minorenne');
-     }else if(eta == 3 ){
-       console.log('over');
-     }
-  });
+  if (eta == 1) {
+    
+    costoBiglietto = costoBiglietto *80/100;
+  } else if (eta == 3) {
+    
+    costoBiglietto = costoBiglietto *60/100;
+  }
+
+  console.log(costoBiglietto);
+});
 
 // annullaBtn.addEventListener('click'),
 //   function() {
